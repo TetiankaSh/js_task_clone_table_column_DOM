ae5567ee1bd1479f515d726b8fe74782b41ebf5a
TetiankaSh <shh.tetiana@gmail.com>
2025-09-13 11:37:21 +0300

    first commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+['thead', 'tbody', 'tfoot'].forEach(sectionName => {
+  const section = table.querySelector(sectionName);
+
+  if (section) {
+    for (const row of section.rows) {
+      const secondCell = row.cells[1]; // index 1 → second column
+      const clonedCell = secondCell.cloneNode(true);
+
+      // insert before last cell in the row
+      row.insertBefore(clonedCell, row.lastElementChild);
+    }
+  }
+});
